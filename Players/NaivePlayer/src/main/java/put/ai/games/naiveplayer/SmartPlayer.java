905b0ac04207f1b0bbe8784898501834f3263bf0
wangobango <puch003@wp.pl>
2019-01-11 18:35:38 +0100

    ifs

--- Players/NaivePlayer/src/main/java/put/ai/games/naiveplayer/SmartPlayer.java
+++ Players/NaivePlayer/src/main/java/put/ai/games/naiveplayer/SmartPlayer.java
@@ -4,6 +4,8 @@
  */
 package put.ai.games.naiveplayer;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 import put.ai.games.game.Board;
@@ -14,6 +16,7 @@ import put.ai.games.game.moves.PlaceMove;
 public class SmartPlayer extends Player {
 
     private Random random = new Random(0xdeadbeef);
+    private ArrayList<PlaceMove> guaranteed = new ArrayList<>();
 
 
     @Override
@@ -26,17 +29,36 @@ public class SmartPlayer extends Player {
     public Move nextMove(Board b) {
         List<Move> moves = b.getMovesFor(getColor());
 
-        for(Move pom:moves){
-            System.out.println(getHeuristicValue(pom,b));
-        }
+        moves.sort(Comparator.comparingInt(o -> getHeuristicValue(o, b)));
 
-        return moves.get(random.nextInt(moves.size()));
+//        PlaceMove top = (PlaceMove)moves.get(moves.size()-1);
+//        guaranteed.add()
+
+
+        return moves.get(moves.size()-1);
 
     }
 
     public int getHeuristicValue(Move m, Board b){
         int value = 0;
         PlaceMove place = (PlaceMove)m;
+
+//        Color right = b.getState(place.getX()+1,place.getY());
+//        if(right == Color.EMPTY){
+//            value+=10;
+//        }
+//        Color bottom = b.getState(place.getX(),place.getY()-1);
+//        if(bottom== Color.EMPTY){
+//            value+=10;
+//        }
+//        Color left = b.getState(place.getX()-1,place.getY());
+//        if(left == Color.EMPTY){
+//            value+=10;
+//        }
+//        Color up = b.getState(place.getX(),place.getY()+1);
+//        if(up == Color.EMPTY){
+//            value+=10;
+//        }
 
         Color right = b.getState(place.getX()+1,place.getY());
         if(right == Color.EMPTY){
@@ -53,6 +75,38 @@ public class SmartPlayer extends Player {
         Color up = b.getState(place.getX(),place.getY()+1);
         if(up == Color.EMPTY){
             value+=10;
+        }
+        Color upup = b.getState(place.getX(),place.getY()+2);
+        if(upup==Color.EMPTY){
+            value +=10;
+        }
+        Color upright = b.getState(place.getX()+1, place.getY()+1);
+        if(upright==Color.EMPTY){
+            value +=10;
+        }
+        Color rightright = b.getState(place.getX()+2, place.getY());
+        if(upright==Color.EMPTY){
+            value +=10;
+        }
+        Color botright = b.getState(place.getX()+1, place.getY()-1);
+        if(botright==Color.EMPTY){
+            value +=10;
+        }
+        Color botbot = b.getState(place.getX()-2, place.getY()-2);
+        if(botbot==Color.EMPTY){
+            value +=10;
+        }
+        Color botleft = b.getState(place.getX()-1, place.getY()-1);
+        if(botleft==Color.EMPTY){
+            value +=10;
+        }
+        Color lefteft = b.getState(place.getX()-2, place.getY());
+        if(lefteft==Color.EMPTY){
+            value +=10;
+        }
+        Color upleft = b.getState(place.getX()-1, place.getY()+1);
+        if(upleft==Color.EMPTY){
+            value +=10;
         }
 
         if( right == getColor() || left == getColor() || up == getColor() || bottom == getColor()){
